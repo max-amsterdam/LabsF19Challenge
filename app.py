ee927e5911dcd431389875a14181fcbe3216fbb8
Max Amsterdam <maxwell.amsterdam@columbia.edu>
2019-09-13 14:33:47 -0400

    Added k-least crowded building groups functionality. Added documenation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from utils import get_building_id_from_name
+from utils import get_building_id_from_name, get_sorted_k_buildings_name_and_percent
 import requests
 
 app = Flask(__name__)
@@ -13,23 +13,70 @@ def main():
 
 @app.route('/information/<int:k>', methods=['GET'])
 def k_least_crowded(k):
-    pass
-    
-@app.route('/information/<string:building_name>', methods=['GET'])
-def building_crowdedness(building_name):
-    building_id = get_building_id_from_name(building_name)
-    if building_id:
-        r = requests.get('{}/latest/building/{}?auth_token={}'.format(base_url, building_id, api_key))
+    """
+    Returns the sorted (on percentage full) k-least crowded building groups to display to the user.
+    If k is greater than the total number of buildings, returns all building groups, sorted based on percent full.
+    """
+    error = ''
+    if k < 0:
+        error = 'Number cannot be negative.'
+    else:
+        # Make API request
+        r = requests.get('{}/latest?auth_token={}'.format(base_url, api_key))
+
+        # Attempt to parse data from API response
         try:
             response_json = r.json()
+            data = response_json.get('data', None)
+        except Exception as e:
+            error = 'API response could not be parsed. Returned status code: {}'.format(r.status_code)
+
+        # Turn raw API response data into sorted strings in following format:
+        # <building_name> is <percent_full>% full.
+        if data:
+            # Get sorted building names and percent full
+            sorted_k_buildings_name_and_percent = get_sorted_k_buildings_name_and_percent(data, k)
+
+            # Convert to string version
+            building_info = list()
+            for building_tuple in sorted_k_buildings_name_and_percent:
+                building_info.append('{} is {}% full'.format(building_tuple[0], building_tuple[1]))
+
+            return render_template('information.html', building_info=building_info)
+    return render_template('information.html', error=error) 
+    
+
+@app.route('/information/<string:building_name>', methods=['GET'])
+def building_crowdedness(building_name):
+    """
+    Returns all of the building groups, along with their percentage full, within the given building name.
+    The building name is not case sensitive, and spaces are given as underscores ('_')
+    """
+    # Get the building id from the given name. The id is needed to make the api call
+    error = ''
+    building_id = get_building_id_from_name(building_name)
+
+    if not building_id:
+        # Given building_name is not a valid name
+        error = 'The given building name is not valid.'
+    else:
+        # Make API call
+        r = requests.get('{}/latest/building/{}?auth_token={}'.format(base_url, building_id, api_key))
+
+        # Attempt to parse data from raw API response
+        try:
+            response_json = r.json()
+            data = response_json.get('data', None)
+        except Exception as e:
+            error = 'API response could not be parsed. Returned status code: {}'.format(r.status_code)
+        if data:
             buildings_and_percentage_full = list()
-            for building in response_json['data']:
+            # Generate list of strings for front end from data
+            for building in data:
                 group_name, percent_full = building['group_name'], building['percent_full']
                 buildings_and_percentage_full.append("{} is {}% full".format(group_name, percent_full))
             return render_template('information.html', building_info=buildings_and_percentage_full)
-        except Exception as e:
-            print("Error while parsing response: {}".format(e))
-    return render_template('information.html', error='Error occurred while getting building data')
+    return render_template('information.html', error=error)
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,6 +1,15 @@
 import json
+from operator import itemgetter
 
 def get_building_id_from_name(building_name):
+    """
+    Gets the building id from a given building name.
+    Uses the 'building_info.txt' file to know which building_name correlates to which id.
+
+    Note: building name is not case sensitive and all white spaces are given as underscores
+
+    Returns None if the given building_name is not found
+    """
     building_info_txt = open('building_info.txt')
     building_info_json = json.load(building_info_txt)
 
@@ -11,6 +20,24 @@ def get_building_id_from_name(building_name):
         if parent_name.lower() == building_name_f:
             return building['parent_id']
     return None
+
+def get_sorted_k_buildings_name_and_percent(data, k):
+    """
+    Converts a list of json objects into a sorted list of at least size k 
+    based on the percent full (ascending order). 
+
+    The return type is a list of tuples, with each tuple corresponding to:
+    (group_name, percent_full)
+    """
+    result_list = list()
+    for building in data:
+        group_name, percent_full = building['group_name'], building['percent_full']
+        result_list.append((group_name, percent_full))
+    result_list.sort(key=itemgetter(1))
+    # Returns the first k elements of the list, unless k is greater than or equal to the 
+    # number of total building groups. In that case, just returns the entire result_list
+    return result_list[:k] if k < len(result_list) else result_list
+            
 
 
 
